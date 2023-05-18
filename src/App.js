6ce4df54051f5418786bb4a3193536a3e31acb37
Anita Sahani <45237162+91anita@users.noreply.github.com>
2023-05-18 14:25:02 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
-import React, { useEffect } from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Emaillist from "./Emaillist";
-import Compose from "./Compose";
-import { useDispatch, useSelector } from "react-redux";
-import { selectSendMessageIsOpen } from "./features/mailSlice";
-import Emaildetail from "./Emaildetail";
-import Login from "./Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { selectUser, signin, signout } from "./features/userSlice";
-import { auth } from "./firebase";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthForm";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
+import ExpenseForm from "./components/Expenseform/ExpenseForm";
+import { useContext } from "react";
+
 function App() {
-  const isMessageOpen = useSelector(selectSendMessageIsOpen);
-  const user = useSelector(selectUser);
-  const dispatch=useDispatch()
-  
-   useEffect(()=> {
-    auth.onAuthStateChanged((user)=> {
-     if(user){
-       dispatch(signin({
-          displayName:user.displayName,
-            email:user.email
-        }))}
-         else{
-         dispatch(signout())
-      }
-    })
-   },[])
+  const authCtx = useContext(AuthContext);
+
+
   return (
-    <Router>
-      {user ? (
-        <div className="App">
-          <Header />
-          <div className="app__body">
-            <Sidebar />
-            <Routes>
-              <Route path="/" element={<Emaillist />} />
-              <Route path="/mail" element={<Emaildetail />} />
-            </Routes>
-          </div>
-          {isMessageOpen && <Compose />}
-        </div>
-      )
-      : (<Login/>)
-        
-      }
-      
-    </Router>
+    
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/expense" exact>
+            {authCtx.isLoggedIn && (<ExpenseForm />)}
+          </Route>
+
+          {!authCtx.isLoggedIn && (
+            <Route path="/auth">
+              <AuthPage />
+            </Route>
+          )}
+
+          <Route path="/profile">
+          {authCtx.isLoggedIn && <UserProfile/>}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" /> }
+          </Route>
+
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Layout>
+   
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAkHFCQccwn--jNVYyZKL4UleLhgQBeCLU",
-  authDomain: "mailboxclient-4f3cf.firebaseapp.com",
-  projectId: "mailboxclient-4f3cf",
-  storageBucket: "mailboxclient-4f3cf.appspot.com",
-  messagingSenderId: "211448024267",
-  appId: "1:211448024267:web:dac7f0e5b3e2c5a9dd5f4e"
+  apiKey: "AIzaSyAOFGfzS7iYgO8Xo8k5H70DjFpF1BNoAr0",
+  authDomain: "frontend-expense.firebaseapp.com",
+  projectId: "frontend-expense",
+  storageBucket: "frontend-expense.appspot.com",
+  messagingSenderId: "482750116591",
+  appId: "1:482750116591:web:4a3f306b22030e7bc8a11a"
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db =firebaseApp.firestore();
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-export {db, auth, provider};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app);
+
+export {auth};
